2a1bca8a74f1bb39609838becff8b61599f55891
Miles Garnett <milesgarnett@gmail.com>
2020-03-29 12:06:54 -0400

    Added shop data

--- src/App.js
+++ src/App.js
@@ -2,20 +2,16 @@ import React from 'react';
 import {Switch, Route } from 'react-router-dom';
 import './App.css';
 
-import Homepage from './pages/homepage/homepage.component.jsx'
+import Homepage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
- );
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
